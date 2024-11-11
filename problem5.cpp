9f901aed211ef75866228a82dae932608e60f6a1
Ozodbek87 <o.sultonov@newuu.uz>
2024-11-11 18:54:10 +0500

    Add files via upload

--- problem5.cpp
+++ problem5.cpp
@@ -5,32 +5,32 @@ int main() {
     string s;
     getline(cin, s);
 
-    int upperCount = 0; 
-    int lowerCount = 0;  
-    string upperChars = ""; 
-    string lowerChars = "";  
+    int upperCount = 0;
+    int lowerCount = 0;
+    string upperChars = "";
+    string lowerChars = "";
 
     for (char c : s) {
-        if (c >= 'A' && c <= 'Z') { 
+        if (c >= 'A' && c <= 'Z') {
             upperCount++;
-            upperChars += c; 
-        } else if (c >= 'a' && c <= 'z') {  
+            upperChars += c;
+        } else if (c >= 'a' && c <= 'z') {
             lowerCount++;
-            lowerChars += c;  
+            lowerChars += c;
         }
     }
-
     cout << upperCount;
     if (upperCount > 0) {
         for (int i = 0; i < upperChars.length(); ++i) {
-            cout << " " << upperChars[i];  
+            cout << " " << upperChars[i];
         }
     }
     cout << endl;
+
     cout << lowerCount;
     if (lowerCount > 0) {
         for (int i = 0; i < lowerChars.length(); ++i) {
-            cout << " " << lowerChars[i];  
+            cout << " " << lowerChars[i];
         }
     }
     cout << endl;
