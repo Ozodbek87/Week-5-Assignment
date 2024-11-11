2dc95e20204a26b6445d6bb2851fbcbbb9669079
Ozodbek87 <o.sultonov@newuu.uz>
2024-11-11 19:00:26 +0500

    Add files via upload

--- problem5.cpp
+++ problem5.cpp
@@ -7,8 +7,8 @@ int main() {
 
     int upperCount = 0;
     int lowerCount = 0;
-    string upperChars = "";
-    string lowerChars = "";
+    string upperChars = " ";
+    string lowerChars = " ";
 
     for (char c : s) {
         if (c >= 'A' && c <= 'Z') {
@@ -22,7 +22,7 @@ int main() {
     cout << upperCount;
     if (upperCount > 0) {
         for (int i = 0; i < upperChars.length(); ++i) {
-            cout << " " << upperChars[i];
+            cout << upperChars[i] <<" ";
         }
     }
     cout << endl;
@@ -30,7 +30,7 @@ int main() {
     cout << lowerCount;
     if (lowerCount > 0) {
         for (int i = 0; i < lowerChars.length(); ++i) {
-            cout << " " << lowerChars[i];
+            cout << lowerChars[i] << " ";
         }
     }
     cout << endl;
